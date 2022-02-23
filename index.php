e05b9d6753a9d32d48b4be65a09c8d078ea2d498
jose041 <joseglez1104@gmail.com>
2022-02-23 11:13:33 -0600

    version 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Definición de la clase
+class Holaenrique {
+
+    // Constructor
+    function Holaenrique() {
+        // Se utiliza para definiciones iniciales
+    }
+
+    // Muestra el texto "Hola Mundo"
+    function Show() {
+        echo "Hola Mi nombre es enrique";
+    }
+}
+
+// Iniciamos la clase
+$obj = new Holaenrique();
+
+// Llamamos al método
+$obj->Show();
